f96197149496b0d4a31210607bc157771a7664f6
Robert Scherer <ich@robertscherer.de>
2015-02-05 17:23:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cake-attachments
+
+CakePHP 3 File Attachments Handling
---
+++ src/Controller/AppController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Attachments\Controller;
+
+use App\Controller\AppController as BaseController;
+
+class AppController extends BaseController
+{
+
+}
